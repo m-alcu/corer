8f88f7e82740de1fbc0f4874ee079b9beaa5931e
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2022-09-26 13:26:51 +0200

    First commit

---
+++ CoreR/CoreR/ClientApp/src/authConfig.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { LogLevel } from "@azure/msal-browser";
+
+/**
+ * Configuration object to be passed to MSAL instance on creation. 
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ */
+export const msalConfig = {
+    auth: {
+        clientId: "e132920e-0b69-4431-87d0-2aee1d046913",
+        authority: "https://login.microsoftonline.com/28be9bb1-d932-4323-9d48-0e662c6549e7",
+        redirectUri: "https://localhost:3050/"
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit: 
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+    //scopes: ["User.Read"]
+    scopes: ["api://df3b930d-44ff-4d6b-aafb-7e45ba3d56f0/access_as_user"]
+};
+
+/**
+ * Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+};
